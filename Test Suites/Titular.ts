8d064096bb997f3e6fb9df72fc4969d0e17937b9
Oscar <Oscar@DESKTOP-QBTR39C>
2020-12-06 15:56:32 -0400

    o Ajustes

---
+++ Test Suites/Titular.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Titular</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>f1c5178e-8cdb-4e0c-801d-01ce9908a279</testSuiteGuid>
+   <testCaseLink>
+      <guid>7d6f9070-2a47-4d1c-910a-29bcc28eb4ab</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Titular/titular</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
